96ccb85870687791773eb2274565c266f4aa6559
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-04-13 03:56:56 +0300

    Improve HomeScreen

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { Text, ScrollView, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+function RenderItem(props) {
+    
+        const item = props.item;
+        
+        if (item != null) {
+            return(
+                <Card>
+                    <Card.Title>{item.name}</Card.Title>
+                    <Card.Divider/>
+                    <Card.Image source={require('./images/uthappizza.png')}/>
+                    <Text style={{margin: 10}}>
+                        {item.description}
+                    </Text>
+                </Card>
+            );
+        }
+        else {
+            return(<View></View>);
+        }
+}
 
 class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          dishes: DISHES,
+          promotions: PROMOTIONS,
+          leaders: LEADERS
+        };
+    }
+
     static navigationOptions = {
-        title: 'Home'
+        title: 'Home',
     };
 
     render() {
+        
         return(
-            <View><Text>Home Component</Text>
-            <Button
-                onPress={() => this.props.navigation.openDrawer()}
-                title="Drawer"
-            />
-            </View>
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]} />
+                <RenderItem item={this.state.promotions.filter((promo) => promo.featured)[0]} />
+                <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]} />
+            </ScrollView>
         );
     }
 }
-
 export default Home;
