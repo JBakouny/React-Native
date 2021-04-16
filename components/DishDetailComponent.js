ba3b8476fb92dc17d4e7d0f51fc8def04a1095e9
Joseph Bakouny <Youssef.Bakouny@usj.edu.lb>
2021-04-16 10:33:47 +0300

    Add favorites

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { Card } from 'react-native-elements';
+import { Card, Icon } from 'react-native-elements';
 import { Text, View, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
       dishes: state.dishes,
-      comments: state.comments
+      comments: state.comments,
+      favorites: state.favorites
     }
   }
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+})
 
 function RenderComments(props) {
 
@@ -51,6 +57,14 @@ function RenderDish(props) {
                     <Text style={{margin: 10}}>
                         {dish.description}
                     </Text>
+                    <Icon
+                        raised
+                        reverse
+                        name = { props.favorite ? 'heart' : 'heart-o' }
+                        type = 'font-awesome'
+                        color = '#f50'
+                        onPress = {() => props.favorite ? console.log('Already favorite') : props.onPress()}
+                        />
                 </Card>
             );
         }
@@ -65,12 +79,16 @@ class DishDetail extends Component {
         title: 'Dish Details'
     };
 
+    markFavorite(dishId) {
+        this.props.postFavorite(dishId);
+    }
+
     render() {
         const dishId = this.props.navigation.getParam('dishId','');
         return(
             <View>
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                   // favorite={this.state.favorites.some(el => el === dishId)}
+                    favorite={this.props.favorites.some(el => el === dishId)}
                     onPress={() => this.markFavorite(dishId)} 
                     />
                 <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
@@ -79,4 +97,4 @@ class DishDetail extends Component {
     }
 }
 
-export default connect(mapStateToProps)(DishDetail);
+export default connect(mapStateToProps, mapDispatchToProps)(DishDetail);
